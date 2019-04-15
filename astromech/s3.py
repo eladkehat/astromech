7b04a92ab669bfe22ad788efdae98e83cdb04cea
Elad Kehat <eladkehat@gmail.com>
2019-04-15 11:37:49 +0300

    S3 module.

---
+++ astromech/s3.py
@@ -0,0 +1,201 @@
+import os
+from typing import Tuple, Union
+import urllib.parse
+
+import boto3
+import botocore
+
+from astromech.logging import logger
+
+_client = None
+"""A boto S3 client, initialized lazily by `client()`.
+
+The client object is global, so that it gets reused between invocations by the lambda
+function container.
+
+Do not use this directly! Instead, use the `client()` function to get an initialized client.
+"""
+
+
+def client() -> botocore.client.BaseClient:
+    """Returns an S3 client object.
+
+    This function always returns the global client object, initializing it if necessary.
+    Use it inside your code whenever you need an S3 client, rather than creating a new one
+    or using the global client object directly.
+
+    Returns:
+        The S3 client object.
+    """
+    global _client
+    if _client is None:
+        _client = boto3.client('s3')
+    return _client
+
+
+def parse_uri(uri: str) -> Tuple[str, str]:
+    """Parse a S3 URI.
+
+    Args:
+        uri: A S3 URI in the format s3://bucket/key.
+
+    Returns:
+        A tuple with the bucket and key.
+    """
+    scheme, bucket, key, _, _, _ = urllib.parse.urlparse(uri)
+    if not scheme == 's3':
+        raise ValueError(f'Not a S3 URI: {uri}')
+    return (bucket, key.lstrip('/'))
+
+
+def to_uri(bucket: str, key: str) -> str:
+    """Construct a S3 URI.
+
+    Args:
+        bucket: The S3 bucket name.
+        key: The S3 key.
+
+    Returns:
+        A S3 URI in the format s3://bucket/key.
+    """
+    return f's3://{bucket}/{key}'
+
+
+def default_path(
+    filename: str, bucket: Union[str, None] = None, key_prefix: Union[str, None] = None
+) -> Tuple[str, str]:
+    """Returns a default S3 path for a given filename using env variables.
+
+    For lambda functions that interact with a specific bucket and / or key prefix,
+    use environment variables to set those bucket and key prefix and then use this
+    function to use them automatically.
+
+    The bucket env variable key is "S3_BUCKET".
+    The key prefix env variable key is "S3_KEY_PREFIX".
+
+    Args:
+        filename: If there is no key prefix, filename is used as the key.
+            If there is a key prefix, filename is used as the suffix, joined to
+            the key prefix with a forward slash ("/").
+        bucket: Override the default S3 bucket name read from the env var.
+            A bucket *must* be supplied - either explicitly or via an env var.
+        key_prefix: Override the default S3 key prefix from the env var.
+            A key prefix is optional.
+
+    Returns:
+        A 2-tuple:
+        - The resolved bucket.
+        - The resolved key.
+
+    Raises:
+        ValueEror if a bucket was not specified and could not be read from the
+        env var.
+    """
+    bucket = bucket or os.environ.get('S3_BUCKET')
+    if not bucket:
+        raise(ValueError('Missing value for the S3 bucket name.'))
+    key_prefix = key_prefix or os.environ.get('S3_KEY_PREFIX')
+    if key_prefix:
+        key = f'{key_prefix.strip("/")}/{filename.lstrip("/")}'
+    else:
+        key = filename.lstrip('/')
+    return (bucket, key)
+
+
+def exists(bucket: str, key: str) -> bool:
+    """Checks whether an object exists on S3.
+
+    Args:
+        bucket: The S3 bucket name.
+        key: The S3 key.
+
+    Returns:
+        True if an object exists at the specified bucket and key, False otherwise.
+        This function may also return False if the client lacks permissions.
+    """
+    try:
+        client().head_object(Bucket=bucket, Key=key)
+    except botocore.client.ClientError:
+        return False
+    else:
+        return True
+
+
+def get_size(bucket: str, key: str) -> int:
+    """Gets the size of an object on S3.
+
+    Args:
+        bucket: The S3 bucket name.
+        key: The S3 key.
+
+    Returns:
+        The size of the object, in bytes.
+    """
+    response = client().head_object(Bucket=bucket, Key=key)
+    return response['ContentLength']
+
+
+def get_bytes(bucket: str, key: str) -> bytes:
+    """Gets the contents of an object on S3.
+
+    Args:
+        bucket: The source S3 bucket name.
+        key: The source S3 key.
+
+    Returns:
+        The object, as a bytes buffer.
+    """
+    logger.debug(f'Reading from s3://{bucket}/{key}')
+    response = client().get_object(Bucket=bucket, Key=key)
+    return response['Body'].read()
+
+
+def get_tags(bucket: str, key: str) -> dict:
+    """Gets an S3 object's tags as a proper dict.
+
+    Converts the boto3 S3 client `get_object_tagging()` TagSet response like:
+    'TagSet': [
+        {
+            'Key': 'my key',
+            'Value': 'my value'
+        },
+    ]
+
+    to the Pythonic:
+    {'my key': 'my value'}
+
+    Args:
+        bucket: The S3 bucket name.
+        key: The S3 key.
+
+    Returns:
+        The object tags, as a dict.
+    """
+    logger.debug(f'Reading tags from s3://{bucket}/{key}')
+    response = client().get_object_tagging(Bucket=bucket, Key=key)
+    return dict((tag['Key'], tag['Value']) for tag in response['TagSet'])
+
+
+def put_bytes(buf: bytes, bucket: str, key: str, tags: dict = {}) -> Tuple[str, str, int]:
+    """Writes a buffer to S3.
+
+    Args:
+        buf: A bytes buffer.
+        bucket: The target S3 bucket name.
+        key: The target S3 key.
+        tags: Tags to assign to the object.
+            Note that allowed characters for tags are Unicode letters, whitespace, and numbers, plus the following
+            special characters: + - = . _ : /
+            For additional characters, use base-64 encoding.
+
+    Returns:
+        A 3-tuple:
+        - The bucket.
+        - The key.
+        - The number of bytes written (length of the buffer).
+    """
+    logger.debug(f'Writing {len(buf)} bytes to s3://{bucket}/{key}')
+
+    tagging = urllib.parse.urlencode(tags)
+    client().put_object(Bucket=bucket, Key=key, Body=buf, Tagging=tagging)
+    return (bucket, key, len(buf))
---
+++ tests/test_s3.py
@@ -0,0 +1,113 @@
+import io
+
+import botocore.client
+import botocore.stub
+import pytest
+
+from astromech import s3
+
+
+@pytest.fixture(scope='module')
+def bucket():
+    return 'test-bucket'
+
+
+@pytest.fixture(scope='module')
+def key():
+    return 'test-key'
+
+
+def test_client():
+    assert s3._client is None
+    client = s3.client()
+    assert isinstance(client, botocore.client.BaseClient)
+    assert s3._client == client
+
+
+def test_parse_uri():
+    uri = 's3://bucket/path/to/file'
+    assert s3.parse_uri(uri) == ('bucket', 'path/to/file')
+
+
+def test_parse_non_s3_uri():
+    for uri in ('http://aws.amazon.com', 'ftp://ftp.example.com/files/'):
+        with pytest.raises(ValueError):
+            s3.parse_uri(uri)
+
+
+def test_to_uri():
+    assert s3.to_uri('my_bucket', 'path/to/my/key') == 's3://my_bucket/path/to/my/key'
+
+
+def test_default_path(monkeypatch):
+    # Bucket + key prefix from the env
+    with monkeypatch.context() as m:
+        m.setenv('S3_BUCKET', 'default-bucket')
+        m.setenv('S3_KEY_PREFIX', 'my/test/prefix')
+        assert s3.default_path('dir/filename.ext') == ('default-bucket', 'my/test/prefix/dir/filename.ext')
+        m.setenv('S3_KEY_PREFIX', '/my/test/prefix/')
+        assert s3.default_path('filename.ext') == ('default-bucket', 'my/test/prefix/filename.ext')
+    # Only a bucket from the env
+    with monkeypatch.context() as m:
+        m.setenv('S3_BUCKET', 'default-bucket')
+        assert s3.default_path('/dir/filename.ext') == ('default-bucket', 'dir/filename.ext')
+        assert s3.default_path('filename.ext', key_prefix='dir') == ('default-bucket', 'dir/filename.ext')
+    # Neither bucket nor key prefix
+    assert s3.default_path('/filename.ext', 'my-bucket', 'dir/') == ('my-bucket', 'dir/filename.ext')
+    assert s3.default_path('/filename.ext', 'my-bucket') == ('my-bucket', 'filename.ext')
+    with pytest.raises(ValueError):
+        s3.default_path('filename.ext')
+
+
+def test_exists(bucket, key):
+    with botocore.stub.Stubber(s3.client()) as stubber:
+        stubber.add_response('head_object', {'ContentLength': 123}, {'Bucket': bucket, 'Key': key})
+        assert s3.exists(bucket, key)
+
+
+def test_does_not_exist(bucket, key):
+    expected_params = {'Bucket': bucket, 'Key': key}
+    with botocore.stub.Stubber(s3.client()) as stubber:
+        stubber.add_client_error(
+            'head_object', service_message='Not Found', http_status_code=404, expected_params=expected_params)
+        assert not s3.exists(bucket, key)
+
+
+def test_get_size(bucket, key):
+    content_length = 473831
+    with botocore.stub.Stubber(s3.client()) as stubber:
+        stubber.add_response('head_object', {'ContentLength': content_length}, {'Bucket': bucket, 'Key': key})
+        assert s3.get_size(bucket, key) == content_length
+
+
+def test_get_bytes(bucket, key):
+    buf = b'Lorem ipsum dolor sit amet'
+    with botocore.stub.Stubber(s3.client()) as stubber:
+        service_response = {'Body': io.BytesIO(buf), 'ContentLength': len(buf)}
+        stubber.add_response('get_object', service_response, {'Bucket': bucket, 'Key': key})
+        assert s3.get_bytes(bucket, key) == buf
+
+
+def test_get_tags(bucket, key):
+    with botocore.stub.Stubber(s3.client()) as stubber:
+        tags = {'key1': 'value1', 'key2': '2'}
+        service_response = {'TagSet': [{'Key': 'key1', 'Value': 'value1'}, {'Key': 'key2', 'Value': '2'}]}
+        stubber.add_response('get_object_tagging', service_response, {'Bucket': bucket, 'Key': key})
+        assert s3.get_tags(bucket, key) == tags
+        # Empty tag set:
+        stubber.add_response('get_object_tagging', {'TagSet': []}, {'Bucket': bucket, 'Key': key})
+        assert s3.get_tags(bucket, key) == {}
+
+
+def test_put_bytes(bucket, key):
+    buf = b'Lorem ipsum dolor sit amet'
+    service_response = {'ETag': '6bcf86bed8807b8e78f0fc6e0a53079d-380'}
+    expected_params = {'Bucket': bucket, 'Key': key, 'Body': buf, 'Tagging': ''}
+    with botocore.stub.Stubber(s3.client()) as stubber:
+        stubber.add_response('put_object', service_response, expected_params)
+        assert s3.put_bytes(buf, bucket, key) == (bucket, key, len(buf))
+        # Now with tags:
+        tags = {'key1': 'value1', 'key2': '2'}
+        expected_params['Tagging'] = 'key1=value1&key2=2'
+        stubber.add_response('put_object', service_response, expected_params)
+        assert s3.put_bytes(buf, bucket, key, tags) == (bucket, key, len(buf))
